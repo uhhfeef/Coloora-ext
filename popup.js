a9b42df3ea82d66e9bf69d965497aed23e057a36
unknown <afeefkhan99@gmail.com>
2023-11-19 11:54:15 +0400

    added payment text for popup and cnfigured color palette popup

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
+const now = new Date();
+const sevenDays = 1000*60*60*24*15 // seven days in milliseconds
+const extpay = ExtPay('phdgnljpgjngdcmkfoakcechbmikjmok')   
+
 document.getElementById('colorWheelBtn').addEventListener('click', function () {
     chrome.runtime.sendMessage({ action: "executeColorWheelScript" });
 });
@@ -7,7 +11,23 @@ document.getElementById('generateColorPaletteBtn').addEventListener('click', fun
 });
 
 document.getElementById('eyedropperBtn').addEventListener('click', function () {
-    chrome.runtime.sendMessage({ action: "executeEyedropper" });
+    extpay.getUser().then(user => {
+        if (user.trialStartedAt && (now - user.trialStartedAt) < fifteenDays) {
+            // user has an active trial
+            chrome.runtime.sendMessage({ action: "executeEyedropper" });
+        } else if (!user.trialStartedAt) {
+            // user has not started a trial
+            extpay.openTrialPage()
+        } else if (user.trialStartedAt && (now - user.trialStartedAt) > fifteenDays) {
+            // user's trial has completed
+            extpay.openPaymentPage()
+        }
+
+        if (user.paid) {
+            // user has paid
+            chrome.runtime.sendMessage({ action: "executeEyedropper" });
+        }
+    })
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -15,6 +35,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const currentVersion = manifestData.version;
     document.getElementById("version").textContent = currentVersion;
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    extpay.getUser().then(user => {
+        if (user.trialStartedAt && (now - user.trialStartedAt) < fifteenDays) {
+            // user has an active trial
+            paymentLink.textContent = 'Free trial activated';
+            document.getElementById('paymentLink').addEventListener('click', function () {
+                extpay.openPaymentPage();
+            });
+        } else if (!user.trialStartedAt) {
+            // user has not started a trial
+            paymentLink.textContent = 'Start free trial';
+            document.getElementById('paymentLink').addEventListener('click', function () {
+                extpay.openTrialPage();
+            });
+        } else if (user.trialStartedAt && (now - user.trialStartedAt) > fifteenDays) {
+            // user's trial has completed
+            paymentLink.textContent = 'Activate Coloora';
+            document.getElementById('paymentLink').addEventListener('click', function () {
+                extpay.openPaymentPage();
+            });
+        }
+
+        if (user.paid) {
+            // user has paid
+            paymentLink.textContent = 'Thx for your support :)';
+        }
+    })
+});
+
 
 // document.getElementById('featureRequestLink').addEventListener('click', function () {
 //     chrome.windows.create({
