cae4204e07acba1b43b01e028ba5ebd5340f1214
predictED-dev <afeefkhan99@gmail.com>
2024-10-23 15:19:42 +0400

    removed

--- old/featureRequest.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    document.getElementById('featureRequestLink').addEventListener('click', function () {
-        console.log("clicked")
-        const googleFormLink = 'https://forms.gle/geJTQToU3DM1qAUn6';
-        window.open(googleFormLink, '_blank');
-    });
-};
